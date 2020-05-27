de1c5e75652f35efa64a96f2b4b7cfce716c9570
monguri <mongry@gmail.com>
2020-05-27 21:52:05 +0900

    main.cppがとりあえず起動できるようにした

--- 03_DisplayHDR10_SutraCopy/DisplayHDR10App.h
+++ 03_DisplayHDR10_SutraCopy/DisplayHDR10App.h
@@ -1,5 +1,7 @@
 #pragma once
-class DisplayHDR10App
+#include "VulkanAppBase.h"
+
+class DisplayHDR10App : public VulkanAppBase
 {
 };
 
--- 03_DisplayHDR10_SutraCopy/main.cpp
+++ 03_DisplayHDR10_SutraCopy/main.cpp
@@ -1,3 +1,26 @@
+#include "DisplayHDR10App.h"
 
 // 本ではプロジェクト設定でリンクに加えてるけどここではvulkan_book_1と同様に#pragmraでリンク指定する
 #pragma comment(lib, "vulkan-1.lib")
+
+const int WindowWidth = 800, WindowHeight = 600;
+const char* AppTile = "DisplayHDR10";
+
+int _stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, AppTile, nullptr, nullptr);
+
+	while (glfwWindowShouldClose(window) == GLFW_FALSE)
+	{
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
+
