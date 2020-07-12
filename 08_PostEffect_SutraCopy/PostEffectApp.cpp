6927ca64edf8c6f7c496d6b8c7afc6257adfb265
monguri <mongry@gmail.com>
2020-07-12 13:58:44 +0900

    レンダーパス作成にbook_utilのAPIを使うようにした

--- 08_PostEffect_SutraCopy/PostEffectApp.cpp
+++ 08_PostEffect_SutraCopy/PostEffectApp.cpp
@@ -165,131 +165,19 @@ void PostEffectApp::Render()
 void PostEffectApp::CreateRenderPass()
 {
 	// 2個のレンダーパスで同じようなコードを書くが、本でソース共通化してないので共通化しない
-	{
-		std::array<VkAttachmentDescription, 2> attachments;
-		VkAttachmentDescription& colorTarget = attachments[0];
-		VkAttachmentDescription& depthTarget = attachments[1];
+	const VkRenderPass& renderPassMain = book_util::CreateRenderPass(
+		m_device,
+		m_swapchain->GetSurfaceFormat().format,
+		VK_FORMAT_D32_SFLOAT
+	);
+	RegisterRenderPass("main", renderPassMain);
 
-		colorTarget = VkAttachmentDescription{};
-		colorTarget.flags = 0;
-		colorTarget.format = m_swapchain->GetSurfaceFormat().format;
-		colorTarget.samples = VK_SAMPLE_COUNT_1_BIT;
-		colorTarget.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-		colorTarget.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-		colorTarget.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-		colorTarget.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-		colorTarget.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		colorTarget.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-
-		depthTarget = VkAttachmentDescription{};
-		depthTarget.flags = 0;
-		depthTarget.format = VK_FORMAT_D32_SFLOAT;
-		depthTarget.samples = VK_SAMPLE_COUNT_1_BIT;
-		depthTarget.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-		depthTarget.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-		depthTarget.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-		depthTarget.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-		depthTarget.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		depthTarget.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-		VkAttachmentReference colorRef{};
-		colorRef.attachment = 0;
-		colorRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-		VkAttachmentReference depthRef{};
-		depthRef.attachment = 1;
-		depthRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-		VkSubpassDescription subpassDesc{};
-		subpassDesc.flags = 0;
-		subpassDesc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-		subpassDesc.inputAttachmentCount = 0;
-		subpassDesc.pInputAttachments = nullptr;
-		subpassDesc.colorAttachmentCount = 1;
-		subpassDesc.pColorAttachments = &colorRef;
-		subpassDesc.pResolveAttachments = nullptr;
-		subpassDesc.pDepthStencilAttachment = &depthRef;
-		subpassDesc.preserveAttachmentCount = 0;
-		subpassDesc.pPreserveAttachments = nullptr;
-
-		VkRenderPassCreateInfo rpCI{};
-		rpCI.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-		rpCI.pNext = nullptr;
-		rpCI.flags = 0;
-		rpCI.attachmentCount = uint32_t(attachments.size());
-		rpCI.pAttachments = attachments.data();
-		rpCI.subpassCount = 1;
-		rpCI.pSubpasses = &subpassDesc;
-		rpCI.dependencyCount = 0;
-		rpCI.pDependencies = nullptr;
-
-		VkRenderPass renderPass;
-		VkResult result = vkCreateRenderPass(m_device, &rpCI, nullptr, &renderPass);
-		ThrowIfFailed(result, "vkCreateRenderPass Failed.");
-		RegisterRenderPass("main", renderPass);
-	}
-
-	// テクスチャ描画用のレンダーパスを準備
-	{
-		std::array<VkAttachmentDescription, 2> attachments;
-		attachments[0] = VkAttachmentDescription{};
-		attachments[0].flags = 0;
-		attachments[0].format = VK_FORMAT_R8G8B8A8_UNORM;
-		attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
-		attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-		attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-		attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-		attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-		attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		attachments[0].finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-		attachments[1] = VkAttachmentDescription{};
-		attachments[1].flags = 0;
-		attachments[1].format = VK_FORMAT_D32_SFLOAT;
-		attachments[1].samples = VK_SAMPLE_COUNT_1_BIT;
-		attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-		attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-		attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-		attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-		attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-		VkAttachmentReference colorRef{};
-		colorRef.attachment = 0;
-		colorRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-		VkAttachmentReference depthRef{};
-		depthRef.attachment = 1;
-		depthRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-		VkSubpassDescription subpassDesc{};
-		subpassDesc.flags = 0;
-		subpassDesc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-		subpassDesc.inputAttachmentCount = 0;
-		subpassDesc.pInputAttachments = nullptr;
-		subpassDesc.colorAttachmentCount = 1;
-		subpassDesc.pColorAttachments = &colorRef;
-		subpassDesc.pResolveAttachments = nullptr;
-		subpassDesc.pDepthStencilAttachment = &depthRef;
-		subpassDesc.preserveAttachmentCount = 0;
-		subpassDesc.pPreserveAttachments = nullptr;
-
-		VkRenderPassCreateInfo rpCI{};
-		rpCI.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-		rpCI.pNext = nullptr;
-		rpCI.flags = 0;
-		rpCI.attachmentCount = uint32_t(attachments.size());
-		rpCI.pAttachments = attachments.data();
-		rpCI.subpassCount = 1;
-		rpCI.pSubpasses = &subpassDesc;
-		rpCI.dependencyCount = 0;
-		rpCI.pDependencies = nullptr;
-
-		VkRenderPass texturePass;
-		VkResult result = vkCreateRenderPass(m_device, &rpCI, nullptr, &texturePass);
-		ThrowIfFailed(result, "vkCreateRenderPass Failed.");
-		RegisterRenderPass("render_target", texturePass);
-	}
+	const VkRenderPass& renderPassRenderTarget = book_util::CreateRenderPassToRenderTarget(
+		m_device,
+		VK_FORMAT_R8G8B8A8_UNORM,
+		VK_FORMAT_D32_SFLOAT
+	);
+	RegisterRenderPass("render_target", renderPassRenderTarget);
 }
 
 void PostEffectApp::PrepareFramebuffers()
